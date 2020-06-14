b81327a9a5b0fc6c621e0527f84d1a4c408e5e94
jamie <jamiedday@hotmail.co.uk>
2020-06-14 22:58:12 +0100

    Disabled test

--- basics/src/test/java/basics/FibonacciSampleTest.java
+++ basics/src/test/java/basics/FibonacciSampleTest.java
@@ -3,9 +3,11 @@
  */
 package basics;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+@Disabled
 class FibonacciSampleTest {
     @Test void testFib() {
         FibonacciSample classUnderTest = new FibonacciSample();
